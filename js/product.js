60c7e35c502e53eaac82f3940f7557f6fa5d648b
nishidhparmar <nishidhparmar1310@gmail.com>
2021-02-28 19:25:27 +0530

    productpage

---
+++ js/product.js
@@ -0,0 +1,5 @@
+const categories_load = document.getElementById('categories_load');
+const categories = document.getElementById('categories');
+categories_load.addEventListener('click',()=>{
+    categories.classList.toggle('cardheight')
+})
